4b0a479f50679f3c4633b67b71b48a969ba2635c
rikriki <frederick.rosales@gmail.com>
2014-06-24 12:37:02 +0400

    second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
+var apiKey  = 'NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1';
+var userID  = 'ionman';
+ (function() {
+ var behanceUserAPI = 'http://www.behance.net/v2/users/'+ userID +'?callback=?&api_key='+ apiKey;
+    
+        $.getJSON(behanceUserAPI, function(user) {
+            
+            var data = JSON.stringify(user);
+            sessionStorage.setItem('behanceUser', data);
+            setUserTemplate();
+            console.log(data)
+        });
 
-$.getJSON('https://www.behance.net/v2/users/rikirosales?api_key=NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1&callback=myCallbackFunction').done(function(data) {
-  console.log(data);
-});
+        function setUserTemplate() {
+	        var userData    = JSON.parse(sessionStorage.getItem('behanceUser')),
+	        getTemplate = $('#profile-template').html(),
+	        template    = Handlebars.compile(getTemplate),
+	        result      = template(userData);
+	        $('div.header').html(result);
+	        console.log(result)
+    	};
+
+})();
+
+
