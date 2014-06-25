d77b8c1f57bd253dbd351b09a6915be8831207c8
Frederick Rosales <frederick.rosales@gmail.com>
2014-06-25 21:18:33 +0400

    tenth

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 var apiKey  = 'NGCVFuJ8w4IWMyLeaGGyg2X01orRKdq1';
-var userID  = 'ionman';
+var userID  = 'rikirosales';
  (function() {
     var behanceUserAPI = 'http://www.behance.net/v2/users/'+ userID +'?callback=?&api_key='+ apiKey;
     var behanceProjectsAPI = 'http://www.behance.net/v2/users/'+ userID +'/projects?callback=?&api_key='+ apiKey;
