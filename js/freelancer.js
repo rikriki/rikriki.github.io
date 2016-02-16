1593ce22256d3e3adfc55965388391428a6e2a97
Frederick <riki_email@yahoo.com>
2016-02-15 22:57:53 -0500

    .

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,12 +15,56 @@ $(function() {
         $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
     }).on("focus", ".floating-label-form-group", function() {
         $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
+    })
+
+    ( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c,function(){
+
+        asdasd
+        var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+ 
+};
+    } );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.navbar-fixed-top',
+    function(){
+        return add:{
+             has: hasClass,
+              add: addClass,
+              remove: removeClass,
+              toggle: toggleClass
+        }
+    }
 })
